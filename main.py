5c7e61e0a259c2258d863861778211dca2cc414e
rafaymerl <rafayabdul0311@gmail.com>
2023-05-07 18:33:17 +0500

    added main file

---
+++ main.py
@@ -0,0 +1,13 @@
+from addition import addition
+from subtraction import subtraction
+from multiply import multiply
+from divide import divide
+
+if __name__=="__main__":
+    a=6
+    b=4
+
+    print (addition (a,b))
+    print (divide (a,b))
+    print (multiply (a,b))
+    print (subtraction (a,b))
